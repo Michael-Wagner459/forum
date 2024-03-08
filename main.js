3b6befff12855d964dfc2f3dcfdfa9d2e548929e
michael-wagner459 <wagner.michael4592@gmail.com>
2024-03-08 12:16:15 -0800

    initial commit and finished product

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 // your code here
+document.getElementById('submit').addEventListener('click', function () {
+  const nameValue = document.getElementById('name').value;
+  const messageValue = document.getElementById('message').value;
+
+  const postDiv = document.querySelector('.posts');
+
+  const newPostDiv = document.createElement('div');
+
+  const newPostTextP = document.createElement('p');
+  const newPostTextNode = document.createTextNode(messageValue);
+  newPostTextP.appendChild(newPostTextNode);
+
+  const newPostNameP = document.createElement('p');
+  const newPostNameNode = document.createTextNode('Posted By: ' + nameValue);
+  newPostNameP.appendChild(newPostNameNode);
+
+  const newPostHR = document.createElement('hr');
+
+  newPostDiv.append(newPostTextP);
+  newPostDiv.append(newPostNameP);
+  newPostDiv.append(newPostHR);
+
+  postDiv.append(newPostDiv);
+
+  console.log(nameValue, messageValue)
+})
